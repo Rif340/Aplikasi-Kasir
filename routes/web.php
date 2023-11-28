78c954fadc8b39b9d6eda20ffa4c3e3a0b3f6f38
Rif340 <arifgrup995@gmail.com>
2023-11-28 08:55:21 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route ;
 
 /*
 |--------------------------------------------------------------------------
