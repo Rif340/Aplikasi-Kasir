70997f6b25cf7e443061eef88b2b7dfeaea7ffcf
Rif340 <arifgrup995@gmail.com>
2024-02-01 09:43:10 +0700

    bismillah

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,6 @@
   <link rel="stylesheet" href="{{asset('assets/css/sidebar.css')}}">
   
 
-  
   <title>Side Navbar</title>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
