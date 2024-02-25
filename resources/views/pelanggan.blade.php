77d111fdb1fdbe55469211c9ec6ec1df060a15ab
To Be Human <67031062+Rif340@users.noreply.github.com>
2024-02-25 18:37:47 +0700

    Update pelanggan.blade.php

--- resources/views/pelanggan.blade.php
+++ resources/views/pelanggan.blade.php
@@ -2,17 +2,58 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kasir.com</title>
-    <link rel="stylesheet" href="{{asset('assets/css/produk.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/boostrap.css')}}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kasir.com</title>
+  <link rel="stylesheet" href="{{asset('assets/css/produk.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/css/boostrap.css')}}">
 </head>
 
 <body>
-    @include('layouts.sidebar')
-    <h1 class="judul">Data Pelanggan</h1>
-    <div class="container">
+  @include('layouts.sidebar')
+
+
+  @if (session()->has('info'))
+  <div class="alert alert-success" role="alert" style="text-align: center; width:50%; margin:auto">
+    {{ session('info') }}
+  </div>
+  @endif
+  @if(session('info'))
+  <script>
+    setTimeout(function () {
+      document.querySelector('.alert').style.display = 'none';
+    }, 3000); // Menyembunyikan alert setelah 3 detik
+  </script>
+  @endif
+
+  @if (session()->has('info2'))
+  <div class="alert alert-success" role="alert" style="text-align: center; width:50%; margin:auto">
+    {{ session('info2') }}
+  </div>
+  @endif
+  @if(session('info2'))
+  <script>
+    setTimeout(function () {
+      document.querySelector('.alert').style.display = 'none';
+    }, 3000); // Menyembunyikan alert setelah 3 detik
+  </script>
+  @endif
+
+  @if (session()->has('info3'))
+  <div class="alert alert-danger" role="alert" style="text-align: center; width:50%; margin:auto">
+    {{ session('info3') }}
+  </div>
+  @endif
+  @if(session('info3'))
+  <script>
+    setTimeout(function () {
+      document.querySelector('.alert').style.display = 'none';
+    }, 3000); // Menyembunyikan alert setelah 3 detik
+  </script>
+  @endif
+
+  <h1 class="judul">Data Pelanggan</h1>
+  <div class="container">
     <a href="{{'tambah_pelanggan'}}" type="button" class="btn btn-success">Tambah pelanggan</a>
     <table style="margin-top: 2rem;" class="table">
       <thead>
@@ -25,13 +66,13 @@
         </tr>
       </thead>
       <tbody>
-      @foreach($pelanggan as $pelanggan)
+        @foreach($pelanggan as $pelanggan)
         <tr>
-        <td>{{$pelanggan->pelanggan_id}}</td>
-        <td>{{$pelanggan->nama_pelanggan}}</td>
-        <td>{{$pelanggan->alamat}}</td>
-        <td>{{$pelanggan->nomor_telepon}}</td>
-        <td class="table-secondary">
+          <td>{{$pelanggan->pelanggan_id}}</td>
+          <td>{{$pelanggan->nama_pelanggan}}</td>
+          <td>{{$pelanggan->alamat}}</td>
+          <td>{{$pelanggan->nomor_telepon}}</td>
+          <td class="table-secondary">
             <a href="/hapus-pelanggan/{{$pelanggan->pelanggan_id}}" class="btn btn-danger">hapus</a>
             <a href="/update-pelanggan/{{$pelanggan->pelanggan_id}}" class="btn btn-success">update</a>
           </td>
@@ -39,6 +80,7 @@
         @endforeach
       </tbody>
     </table>
+    <a href="trash-pelanggan" type="submit">Pelanggan Yang Dihapus</a>
   </div>
 </body>
 
